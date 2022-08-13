65594bbdb83e3232bbc9a6d6fa07df72008a1671
aabyodj <aabyodj@gmail.com>
2022-08-13 19:21:16 +0300

    Add read config from environment variables feature

--- src/main/java/by/aab/isp/config/Config.java
+++ src/main/java/by/aab/isp/config/Config.java
@@ -2,17 +2,28 @@ package by.aab.isp.config;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Map;
 import java.util.Properties;
 
 public class Config {
     
     private static final String CONFIG_FILE_NAME = "/application.properties";
+    private static final Map<String, String> ENV_VARIABLE_NAMES = Map.of(
+            "ISP_DB_URL", "db.url",
+            "ISP_DB_USER", "db.user",
+            "ISP_DB_PASSWORD", "db.password",
+            "ISP_DB_POOLSIZE", "db.poolsize"
+    );
     
     private final Properties properties = new Properties();
     
     private Config() {
         try (InputStream in = getClass().getResourceAsStream(CONFIG_FILE_NAME)) {
             properties.load(in);
+            ENV_VARIABLE_NAMES.forEach((env, prop) -> {
+                String value = System.getenv(env);
+                if (value != null) properties.setProperty(prop, value);
+            });
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
