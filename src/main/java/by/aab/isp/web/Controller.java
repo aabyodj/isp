b7b740e9b22c81e0ba77cb9f82559fd8f3957ba2
aabyodj <aabyodj@gmail.com>
2022-08-17 21:41:47 +0300

    Use Log4J2 in Controller

--- src/main/java/by/aab/isp/web/Controller.java
+++ src/main/java/by/aab/isp/web/Controller.java
@@ -7,9 +7,11 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.log4j.Log4j2;
 
 import java.io.IOException;
 
+@Log4j2
 @WebServlet("/")
 public final class Controller extends HttpServlet {
 
@@ -17,32 +19,56 @@ public final class Controller extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        log.trace("Processing GET request '" + req.getRequestURL() + "'");
         processRequest(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        log.trace("Processing POST request '" + req.getRequestURL() + "'");
         processRequest(req, resp);
     }
 
-    private static void processRequest(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Command command = (Command) req.getAttribute("command");
-        String path = command.apply(req);
-        if (path.startsWith(SCHEMA_REDIRECT)) {
-            resp.sendRedirect(path.substring(SCHEMA_REDIRECT.length()));
-        } else {
-            req.getRequestDispatcher(path).forward(req, resp);
+    private void processRequest(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            Command command = (Command) req.getAttribute("command");
+            String path = command.apply(req);
+            if (path.startsWith(SCHEMA_REDIRECT)) {
+                String redirect = path.substring(SCHEMA_REDIRECT.length());
+                log.trace("Redirecting to '" + redirect + "'");
+                resp.sendRedirect(redirect);
+            } else {
+                log.trace("Forwarding to '" + path + "'");
+                req.getRequestDispatcher(path).forward(req, resp);
+            }
+        } catch (Throwable e) {
+            log.error("Error processing request '" + req.getRequestURL() + "'", e);
+            throw e;
         }
     }
 
     @Override
     public void init() throws ServletException {
-        CommandFactory.getInstance().init();
+        try {
+            log.trace("Initializing the controller...");
+            CommandFactory.getInstance().init();
+            log.info("Initialization complete");
+        } catch (Throwable e) {
+            log.fatal("Failed to initialize", e);
+            throw e;
+        }
     }
 
     @Override
     public void destroy() {
-        CommandFactory.getInstance().destroy();
+        try {
+            log.trace("Destroying the controller...");
+            CommandFactory.getInstance().destroy();
+            log.info("Destruction complete");
+        } catch (Throwable e) {
+            log.error("Failed to destroy the controller", e);
+            throw e;
+        }
     }
     
 }
