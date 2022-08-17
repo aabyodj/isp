77beb4506f0ba3d612dcf464bd5ff5b761947b47
aabyodj <aabyodj@gmail.com>
2022-08-17 22:09:49 +0300

    Use Log4J2 in AuthorizationFilter

--- src/main/java/by/aab/isp/web/filter/AuthorizationFilter.java
+++ src/main/java/by/aab/isp/web/filter/AuthorizationFilter.java
@@ -7,27 +7,33 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.log4j.Log4j2;
 
 import java.io.IOException;
 
+@Log4j2
 public class AuthorizationFilter extends HttpFilter {
 
     @Override
     protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
         Command command = (Command) req.getAttribute("command");
         if (command.isAllowedForAnonymous()) {
+            log.trace("Not requiring authentication for command '" + command.getClass().getSimpleName() + "'");
             super.doFilter(req, res, chain);
             return;
         }
         User user = (User) req.getAttribute("activeUser");
         if (null == user) {
+            log.trace("Rejected anonymous user for command '" + command.getClass().getSimpleName() + "'");
             res.sendError(HttpServletResponse.SC_UNAUTHORIZED);
             return;
         }
         if (command.isAllowedForUser(user)) {
+            log.warn("Accepted user " + user + " for command '" + command.getClass().getSimpleName() + "'");
             super.doFilter(req, res, chain);
             return;
         }
+        log.warn("Rejected user " + user + " for command '" + command.getClass().getSimpleName() + "'");
         res.sendError(HttpServletResponse.SC_FORBIDDEN);
     }
 
