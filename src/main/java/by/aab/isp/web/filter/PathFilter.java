c81cfcb4b908aac70be71274c731069497e47735
aabyodj <aabyodj@gmail.com>
2022-08-17 21:53:08 +0300

    Use Log4J2 in PathFilter

--- src/main/java/by/aab/isp/web/filter/PathFilter.java
+++ src/main/java/by/aab/isp/web/filter/PathFilter.java
@@ -5,10 +5,12 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.log4j.Log4j2;
 
 import java.io.IOException;
 import java.util.Set;
 
+@Log4j2
 public class PathFilter extends HttpFilter {
 
     private static final Set<String> ALLOWED_PATH_SET = Set.of(
@@ -20,9 +22,11 @@ public class PathFilter extends HttpFilter {
         String path = req.getServletPath();
         path = path.substring(0, path.lastIndexOf('/') + 1);
         if (ALLOWED_PATH_SET.contains(path)) {
+            log.trace("Accepted path '" + path + "'");
             chain.doFilter(req, res);
-            return;
+        } else {
+            log.warn("Rejected path '" + path +"'");
+            res.sendError(HttpServletResponse.SC_NOT_FOUND);
         }
-        res.sendError(HttpServletResponse.SC_NOT_FOUND);
     }
 }
