211481d03d276faeeefa969a2cb444878618a352
aabyodj <aabyodj@gmail.com>
2022-08-17 22:36:19 +0300

    Use Log4J2 in CommandFilter

--- src/main/java/by/aab/isp/web/filter/CommandFilter.java
+++ src/main/java/by/aab/isp/web/filter/CommandFilter.java
@@ -7,23 +7,39 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.log4j.Log4j2;
 
 import java.io.IOException;
+import java.util.NoSuchElementException;
 
+@Log4j2
 public class CommandFilter extends HttpFilter {
 
     private CommandFactory commandFactory;
 
     @Override
     public void init() throws ServletException {
-        commandFactory = CommandFactory.getInstance();
+        try {
+            log.trace("Initializing...");
+            commandFactory = CommandFactory.getInstance();
+            log.info("Initialization complete");
+        } catch (Throwable e) {
+            log.fatal("Failed to initialize", e);
+            throw e;
+        }
     }
 
     @Override
     protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
         String commandName = req.getParameter("action");
-        Command command = commandFactory.getCommand(commandName);
-        req.setAttribute("command", command);
-        chain.doFilter(req, res);
+        try {
+            Command command = commandFactory.getCommand(commandName);
+            req.setAttribute("command", command);
+            log.trace("Found command '" + command.getClass().getName() + "' for name '" + commandName + "'");
+            chain.doFilter(req, res);
+        } catch (NoSuchElementException e) {
+            log.error("Unknown command '" + commandName + "'");
+            res.sendError(HttpServletResponse.SC_BAD_REQUEST);
+        }
     }
 }
