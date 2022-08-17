de59f48b37ff87597b0beb4712bf5f952ce72c11
aabyodj <aabyodj@gmail.com>
2022-08-17 23:18:56 +0300

    User Log4J2 in UserSessionFilter

--- src/main/java/by/aab/isp/web/filter/UserSessionFilter.java
+++ src/main/java/by/aab/isp/web/filter/UserSessionFilter.java
@@ -11,16 +11,25 @@ import jakarta.servlet.http.HttpFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import lombok.extern.log4j.Log4j2;
 
 import java.io.IOException;
 
+@Log4j2
 public class UserSessionFilter extends HttpFilter {
 
     private UserService userService;
 
     @Override
     public void init() throws ServletException {
-        userService = ServiceFactory.getInstance().getService(UserService.class);
+        try {
+            log.trace("Initializing...");
+            userService = ServiceFactory.getInstance().getService(UserService.class);
+            log.info("Initialization complete");
+        } catch (Throwable e) {
+            log.fatal("Failed to initialize", e);
+            throw e;
+        }
     }
 
     @Override
@@ -31,11 +40,18 @@ public class UserSessionFilter extends HttpFilter {
 
     private void setUserAttribute(HttpServletRequest req) {
         HttpSession session = req.getSession(false);
-        if (null == session) return;
+        if (null == session) {
+            log.trace("Anonymous user. No session found");
+            return;
+        }
         Long userId = (Long) session.getAttribute("userId");
-        if (null == userId) return;
+        if (null == userId) {
+            log.trace("Anonymous user. Session id=" + session.getId());
+            return;
+        }
         User user = userService.getById(userId);
         req.setAttribute("activeUser", user);
+        log.trace(user + ". Session id=" + session.getId());
         if (user instanceof Customer) {
             req.setAttribute("activeCustomer", user);
         } else if (user instanceof Employee) {
