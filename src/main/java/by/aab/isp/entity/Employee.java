76e63ebd585f1a660b262c774f9c6c5c982b56ab
aabyodj <aabyodj@gmail.com>
2022-08-18 10:52:38 +0300

    Include inherited toString() for descendants of User

--- src/main/java/by/aab/isp/entity/Employee.java
+++ src/main/java/by/aab/isp/entity/Employee.java
@@ -2,9 +2,11 @@ package by.aab.isp.entity;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 @Data
 @EqualsAndHashCode(callSuper=true)
+@ToString(callSuper = true)
 public class Employee extends User {
 
     private Role role;
