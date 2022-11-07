51e06cb57a0e94d31d3d0e775eb5e823bd1ff3ea
aabyodj <aabyodj@gmail.com>
2022-11-07 23:20:03 +0300

    Remove caching of TarifViewDto

--- src/main/java/by/aab/isp/converter/tariff/TariffToTariffViewDtoConverter.java
+++ src/main/java/by/aab/isp/converter/tariff/TariffToTariffViewDtoConverter.java
@@ -1,8 +1,5 @@
 package by.aab.isp.converter.tariff;
 
-import java.util.Map;
-import java.util.WeakHashMap;
-
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
@@ -16,26 +13,18 @@ import lombok.RequiredArgsConstructor;
 public class TariffToTariffViewDtoConverter implements Converter<Tariff, TariffViewDto> {
 
     private final FormatUtil formatUtil;
-    private final Map<Tariff, TariffViewDto> cache = new WeakHashMap<>();
 
     @Override
     public TariffViewDto convert(Tariff entity) {
-        synchronized (cache) {
-            TariffViewDto dto = cache.get(entity);
-            if (null == dto) {
-                dto = TariffViewDto.builder()
-                    .id(entity.getId())
-                    .name(entity.getName())
-                    .description(entity.getDescription())
-                    .bandwidth(formatUtil.formatBandwidth(entity.getBandwidth()))
-                    .includedTraffic(formatUtil.formatTraffic(entity.getIncludedTraffic()))
-                    .price(entity.getPrice().toString())
-                    .active(entity.isActive())
-                    .build();
-                cache.put(entity, dto);
-            }
-            return dto;
-        }
+        return TariffViewDto.builder()
+                .id(entity.getId())
+                .name(entity.getName())
+                .description(entity.getDescription())
+                .bandwidth(formatUtil.formatBandwidth(entity.getBandwidth()))
+                .includedTraffic(formatUtil.formatTraffic(entity.getIncludedTraffic()))
+                .price(entity.getPrice().toString())
+                .active(entity.isActive())
+                .build();
     }
 
 }
